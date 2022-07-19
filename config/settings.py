d89238584562a453a6daff036fd2a52af45a38b4
Seth LaFountain <seth.d.lafountain@gmail.com>
2022-07-18 19:34:13 -0700

    after class 2 113

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 # LOGOUT_REDIRECT_URL = 'home'
+
+#these settings are obtained from the email provider(sendgrid)
+#port 25 is classically the SMTP port, 587 for TLS
+EMAIL_BACKEND = 'django.core.mail.backends.%s.EmailBackend' % env.str("EMAIL_BACKEND", default="smtp")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = env.str("EMAIL_HOST")
+EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
